0e50c3f652743e17c2af271ebd38b9cd9faa5ab4
Mahesh Margaje <143204679+mahesh-margaje@users.noreply.github.com>
2023-08-28 15:10:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About'
 import Contact from './components/Contact'
 import BlogsList from './components/BlogsList'
 import NotFound from './components/NotFound'
+import BlogItemDetails from './components/BlogItemDetails'
 
 import './App.css'
 
@@ -15,6 +16,7 @@ const App = () => (
       <Route exact path="/" component={BlogsList} />
       <Route exact path="/about" component={About} />
       <Route exact path="/contact" component={Contact} />
+      <Route path="/blogs/:id" component={BlogItemDetails} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
